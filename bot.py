12954b00116af4f3e79fde24aee4c6596b50db72
Casey Johnson <casey.johnson1615@gmail.com>
2019-03-19 14:04:19 -0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,9 @@
+import time
+import twitter
+
+api = twitter.Api(	consumer_key = "WmJlz4YogcfCFHf78G45JmQ4W",
+					consumer_secret = "2IEun9ZcYnBhwF30di4X3PxnAh8gA76KrauT1Tst0HGdOwrlN9",
+					access_token_key = "1108033076167364608-F7pygEJqqu50bESbG7Pjw60z1gjHf6",
+					access_token_secret = "ZhoUJbggGsu1F06pTgc7vPaZ3C7LEq2GMvSBhkYKfrMHV")
+
+api.PostUpdate('No')
