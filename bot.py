1cdeb27130068eeba1c5d918eb67ee173f32102d
Casey Johnson <casey.johnson1615@gmail.com>
2019-03-20 12:46:38 -0400

    Fixed analytics

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ api = twitter.Api(	consumer_key = "WmJlz4YogcfCFHf78G45JmQ4W",
 					consumer_secret = "2IEun9ZcYnBhwF30di4X3PxnAh8gA76KrauT1Tst0HGdOwrlN9",
 					access_token_key = "1108033076167364608-F7pygEJqqu50bESbG7Pjw60z1gjHf6",
 					access_token_secret = "ZhoUJbggGsu1F06pTgc7vPaZ3C7LEq2GMvSBhkYKfrMHV")
-
+					
+old_followers = 0
 while True:
 #analytics DM
-	old_followers = 0
 
 	account = api.GetUser(user_id=1108033076167364608)
 	rts = account.status.retweet_count
