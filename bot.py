4d77a6ad1ebc8af2052406c867cdb4173e181ad2
Casey Johnson <casey.johnson1615@gmail.com>
2019-08-01 21:07:43 -0400

    removed creds from code lol

--- bot.py
+++ bot.py
@@ -1,22 +1,22 @@
 import time, twitter, os
 
 try:
-	consumer_key = os.environ['CONSUMER_KEY']
-	consumer_secret = os.environ['CONSUMER_SECRET']
-	access_token_key = os.environ['ACCESS_TOKEN_KEY']
-	access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+	consumer_key = str(os.environ['CONSUMER_KEY'])
+	consumer_secret = str(os.environ['CONSUMER_SECRET'])
+	access_token_key = str(os.environ['ACCESS_TOKEN_KEY'])
+	access_token_secret = str(os.environ['ACCESS_TOKEN_SECRET'])
 except Exception as ex:
 	print("Failed to load credentials.")
+	exit()
 
 try:
-	api = twitter.Api(	consumer_key = "WmJlz4YogcfCFHf78G45JmQ4W",
-						consumer_secret = "2IEun9ZcYnBhwF30di4X3PxnAh8gA76KrauT1Tst0HGdOwrlN9",
-						access_token_key = "1108033076167364608-F7pygEJqqu50bESbG7Pjw60z1gjHf6",
-						access_token_secret = "ZhoUJbggGsu1F06pTgc7vPaZ3C7LEq2GMvSBhkYKfrMHV")
+	api = twitter.Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token_key=ACCESS_TOKEN_KEY, access_token_secret=ACCESS_TOKEN_SECRET)
 except Exception as ex:
 	print("Failed to authenticate to Twitter.")
+	exit()
 
 try:
 	api.PostUpdate('No.')
 except Exception as ex:
 	print("Failed to post update to Twitter.")
+	exit()
