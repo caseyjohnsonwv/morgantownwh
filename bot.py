0b6f0cb1f0ee41048601f9b944a0a2fce4200cc8
Casey Johnson <casey.johnson1615@gmail.com>
2019-08-01 21:02:07 -0400

    initial commit

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
-import time
-import twitter
+import time, twitter, os
 
-api = twitter.Api(	consumer_key = "WmJlz4YogcfCFHf78G45JmQ4W",
-					consumer_secret = "2IEun9ZcYnBhwF30di4X3PxnAh8gA76KrauT1Tst0HGdOwrlN9",
-					access_token_key = "1108033076167364608-F7pygEJqqu50bESbG7Pjw60z1gjHf6",
-					access_token_secret = "ZhoUJbggGsu1F06pTgc7vPaZ3C7LEq2GMvSBhkYKfrMHV")
-					
-while True:
-	time.sleep(64800)
+try:
+	consumer_key = os.environ['CONSUMER_KEY']
+	consumer_secret = os.environ['CONSUMER_SECRET']
+	access_token_key = os.environ['ACCESS_TOKEN_KEY']
+	access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+except Exception as ex:
+	print("Failed to load credentials.")
+
+try:
+	api = twitter.Api(	consumer_key = "WmJlz4YogcfCFHf78G45JmQ4W",
+						consumer_secret = "2IEun9ZcYnBhwF30di4X3PxnAh8gA76KrauT1Tst0HGdOwrlN9",
+						access_token_key = "1108033076167364608-F7pygEJqqu50bESbG7Pjw60z1gjHf6",
+						access_token_secret = "ZhoUJbggGsu1F06pTgc7vPaZ3C7LEq2GMvSBhkYKfrMHV")
+except Exception as ex:
+	print("Failed to authenticate to Twitter.")
+
+try:
 	api.PostUpdate('No.')
-	time.sleep(21600)
-	
-#after pushing, call 'heroku ps:scale worker=1' to init one instance
+except Exception as ex:
+	print("Failed to post update to Twitter.")
